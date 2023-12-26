670cefff4984e47ba8cf9b454dd7e2808d4d802d
Anastasiia <nastya.kll@yandex.ru>
2023-12-27 00:26:49 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+  <div class="app">
+    
+  </div>
+</template>
+
+<style>
+.app {
+  padding: 20px;
+  display: flex;
+  justify-content: center;
+}
+</style>
